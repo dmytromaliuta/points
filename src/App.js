d3b7292e970e0d356adc37a01ee807813acd040e
Dmytro Maliuta <dmytromaliuta@gmail.com>
2020-03-30 12:19:08 +0300

    date of game

--- src/App.js
+++ src/App.js
@@ -27,6 +27,29 @@ class App extends React.Component {
       .then(res => {
         const winners = res.data.reverse().splice(0, 10);
         winners.map((item) => {
+            if(isNaN((Date.parse(item.date)))) {
+              let date = item.date.split(/\;|\,| /);
+              let year = date[date.length - 1][2] + date[date.length - 1][3];
+              let month = date [3];
+              switch(month) {
+                case 'January': month = '01'; break;
+                case 'February': month = '02'; break;
+                case 'March': month = '03'; break;
+                case 'April': month = '04'; break;
+                case 'May': month = '05'; break;
+                case 'June': month = '06'; break;
+                case 'July': month = '07'; break;
+                case 'August': month = '08'; break;
+                case 'September': month = '09'; break;
+                case 'October': month = '10'; break;
+                case 'November': month = '11'; break;
+                case 'December': month = '12'; break;
+              }
+              let day = date[2];
+              let time = date[0];
+              item.date = time + ' ' + day + '.' + month + '.' + year;
+              return
+            };
             let dateMilisec = Date.parse(item.date);
             let newDate = new Date(dateMilisec);
             let year = newDate.getFullYear() % 100;
